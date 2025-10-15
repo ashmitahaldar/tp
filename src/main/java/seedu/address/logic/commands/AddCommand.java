cb3dfb22c3a87a9e31613ae24f77713a8e6913af
ngmh <ngmhcartoon@gmail.com>
2025-10-15 17:38:49 +0800

    Fix integration with edit command

--- src/main/java/seedu/address/logic/commands/AddCommand.java
+++ src/main/java/seedu/address/logic/commands/AddCommand.java
@@ -32,7 +32,7 @@ public class AddCommand extends Command {
             + "Example: " + COMMAND_WORD + " "
             + PREFIX_NAME + "John Doe "
             + PREFIX_PHONE + "98765432 "
-            + PREFIX_TELEGRAM + "@john_doe"
+            + PREFIX_TELEGRAM + "@john_doe "
             + PREFIX_EMAIL + "johnd@example.com "
             + PREFIX_ADDRESS + "311, Clementi Ave 2, #02-25 "
             + PREFIX_TAG + "friends "
