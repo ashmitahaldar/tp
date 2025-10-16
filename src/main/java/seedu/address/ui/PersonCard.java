b562bb7d283615e29a1d3b0e555d865f08561127
ngmh <ngmhcartoon@gmail.com>
2025-10-16 13:03:23 +0800

    Add conditional render to Telegram handle field

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -52,7 +52,15 @@ public class PersonCard extends UiPart<Region> {
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
+        boolean validTelegram = person.getTelegramHandle().valid;
         telegram.setText(person.getTelegramHandle().value);
+        if (validTelegram) {
+            telegram.setVisible(true);
+            telegram.setManaged(true);
+        } else {
+            telegram.setVisible(false);
+            telegram.setManaged(false);
+        }
         address.setText(person.getAddress().value);
         email.setText(person.getEmail().value);
         person.getTags().stream()
